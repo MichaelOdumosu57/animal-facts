a3665ceb137dbb3ff9ff3a5717e001ce9d8c716e
Restop-2345 <michaelodumosu57@gmail.com>
2021-08-20 18:03:21 -0400

    [UPDATE] bolierplate add images

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
+// @ts-nocheck
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { animals } from './animals';
+
+
+
+const title = "";
+
+const displayFact = (e:Event)=>{
+    const facts = animals[e.target.alt].facts 
+    const chosenFact = facts[Math.floor(Math.random() * facts.length)]
+    document.querySelector("#fact").innerHTML = chosenFact
+}
+
+// convert animats object to array
+const images = Object.entries(animals)
+.map((x,i)=>{
+    let [keyx,valx] =x;
+    let myObj =  {
+        key:keyx,
+        className:'animal',
+        alt:keyx,
+        src:valx.image,
+        "aria-label":keyx,
+        role:"button",
+        onClick:displayFact
+    }
+    return <img {...myObj}/>
+
+})
+// 
+
+const background = <img className="background" alt="ocean" src ="images/ocean.jpg"/>;
+const showBackground = true;
+const animalFacts = (
+  <div>
+      <h1>{title === "" ?"Click an animal for a fun fact" :title}</h1>
+      {showBackground && background}
+      <div className="animals">
+          {images}
+      </div>
+      <p id ="fact"></p>
+  </div>
+);
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return animalFacts
 }
 
 export default App;
